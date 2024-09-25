4c04d40dcf18e4c383e5f58af1db7f151f410617
UnknownMaillet <62292183+arnaudmaillet@users.noreply.github.com>
2024-09-25 18:10:47 +0200

    Feat(map): rendering map points

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import MapboxGL from '@rnmapbox/maps';
+import MapboxGL, { Images } from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+import iconChat from '../assets/icon-chat.png';
+import locations from '../data/locations.json';
 
 MapboxGL.setAccessToken(process.env.MAPBOX_ACCESS_TOKEN || '');
 
 const Map = () => {
+
+    const locationsCollection = featureCollection(locations.map((location) => point([location.longitude, location.latitude])));
+
     return (
         <View style={styles.container}>
             <MapboxGL.MapView style={styles.map}>
                 <MapboxGL.Camera followUserLocation followZoomLevel={16}></MapboxGL.Camera>
                 <MapboxGL.LocationPuck puckBearingEnabled puckBearing='heading' pulsing={{ isEnabled: true }} />
+
+                <MapboxGL.ShapeSource id="points" shape={locationsCollection}>
+                    <MapboxGL.SymbolLayer id="point" style={{
+                        iconImage: 'iconChat',
+                        iconSize: 0.05,
+                    }} />
+                    <Images images={{ iconChat }} />
+                </MapboxGL.ShapeSource>
             </MapboxGL.MapView>
         </View>
     );
