66bc08f25a8a991d26b49cd1620f812c36108228
UnknownMaillet <62292183+arnaudmaillet@users.noreply.github.com>
2024-09-26 17:33:26 +0200

    Feat(map): add to data and rendering some medias & multiple clustering

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,13 +3,22 @@ import { StyleSheet, View } from 'react-native';
 import MapboxGL, { MapView, Images, SymbolLayer, CircleLayer, ShapeSource, Camera, LocationPuck, FillExtrusionLayer } from '@rnmapbox/maps';
 import { featureCollection, point } from '@turf/helpers';
 import iconChat from '../assets/icon-chat.png';
+import iconPhoto from '../assets/icon-photo.png';
+import iconVideo from '../assets/icon-video.png';
+import iconMusic from '../assets/icon-music.png';
+import iconLive from '../assets/icon-live.png';
 import locations from '../data/locations.json';
 
 MapboxGL.setAccessToken('sk.eyJ1IjoibWFpbGxldGFyIiwiYSI6ImNtMTgxZ3l3bDB3MmsybnNjOTJ0cWozZWcifQ.dgWa21wpx6rWnfsnmjQMNQ');
 
 const Map = () => {
 
-    const locationsCollection = featureCollection(locations.map((location) => point([location.longitude, location.latitude])));
+    const locationsChats = featureCollection(locations.chats.map((location) => point([location.longitude, location.latitude])));
+    const locationsPhotos = featureCollection(locations.photos.map((location) => point([location.longitude, location.latitude])));
+    const locationsVideos = featureCollection(locations.videos.map((location) => point([location.longitude, location.latitude])));
+    const locationsMusics = featureCollection(locations.musics.map((location) => point([location.longitude, location.latitude])));
+    const locationLives = featureCollection(locations.lives.map((location) => point([location.longitude, location.latitude])));
+
 
     return (
         <View style={styles.container}>
@@ -28,15 +37,72 @@ const Map = () => {
                     }}
                 />
 
-                <ShapeSource id="points" cluster shape={locationsCollection}>
-                    <SymbolLayer id="cluster-count"
+                <ShapeSource id="pointsChats" cluster shape={locationsChats}>
+                    <SymbolLayer id="clusterChats-count"
                         filter={['has', 'point_count']}
                         style={{
                             textField: ['get', 'point_count_abbreviated'],
                             textColor: '#000',
                             textSize: 12,
                         }} />
-                    <CircleLayer id="cluster"
+                    <CircleLayer id="clusterChats"
+                        filter={['has', 'point_count']}
+                        style={{
+                            circlePitchAlignment: 'map',
+                            circleColor: '#ffc107',
+                            circleRadius: ['interpolate', ['linear'], ['get', 'point_count'], 2, 15, 5, 17, 10, 20, 15, 25, 20, 30],
+                            circleOpacity: 0.4,
+                            circleStrokeColor: '#ffc107',
+                            circleStrokeWidth: 1,
+                        }} />
+                    <SymbolLayer id="pointChat"
+                        filter={['!', ['has', 'point_count']]}
+                        style={{
+                            iconImage: 'iconChat',
+                            iconSize: 0.04,
+                            iconAnchor: 'bottom-left',
+                        }} />
+                    <Images images={{ iconChat }} />
+                </ShapeSource>
+
+                <ShapeSource id="pointsPhotos" cluster shape={locationsPhotos}>
+                    <SymbolLayer id="clusterPhotos-count"
+                        filter={['has', 'point_count']}
+                        style={{
+                            textField: ['get', 'point_count_abbreviated'],
+                            textColor: '#000',
+                            textSize: 12,
+                        }} />
+                    <CircleLayer id="clusterPhotos"
+                        filter={['has', 'point_count']}
+                        style={{
+                            circlePitchAlignment: 'map',
+                            circleColor: '#f44336',
+                            circleRadius: ['interpolate', ['linear'], ['get', 'point_count'], 2, 15, 5, 17, 10, 20, 15, 25, 20, 30],
+                            circleOpacity: 0.4,
+                            circleStrokeColor: '#f44336',
+                            circleStrokeWidth: 1,
+                        }} />
+                    <SymbolLayer id="pointPhoto"
+                        filter={['!', ['has', 'point_count']]}
+                        style={{
+                            iconImage: 'iconPhoto',
+                            iconSize: 0.04,
+                            iconAnchor: 'bottom-left',
+                            iconOpacity: 0.8,
+                        }} />
+                    <Images images={{ iconPhoto }} />
+                </ShapeSource>
+
+                <ShapeSource id="pointsVideos" cluster shape={locationsVideos}>
+                    <SymbolLayer id="clusterVideos-count"
+                        filter={['has', 'point_count']}
+                        style={{
+                            textField: ['get', 'point_count_abbreviated'],
+                            textColor: '#000',
+                            textSize: 12,
+                        }} />
+                    <CircleLayer id="clusterVideos"
                         filter={['has', 'point_count']}
                         style={{
                             circlePitchAlignment: 'map',
@@ -46,14 +112,65 @@ const Map = () => {
                             circleStrokeColor: '#2196f3',
                             circleStrokeWidth: 1,
                         }} />
-                    <SymbolLayer id="point"
+                    <SymbolLayer id="pointVideo"
                         filter={['!', ['has', 'point_count']]}
                         style={{
-                            iconImage: 'iconChat',
-                            iconSize: 0.05,
+                            iconImage: 'iconVideo',
+                            iconSize: 0.04,
                             iconAnchor: 'bottom-left',
+                            iconOpacity: 0.8,
                         }} />
-                    <Images images={{ iconChat }} />
+                    <Images images={{ iconVideo }} />
+                </ShapeSource>
+
+                <ShapeSource id="pointsMusics" cluster shape={locationsMusics}>
+                    <SymbolLayer id="clusterMusics-count"
+                        filter={['has', 'point_count']}
+                        style={{
+                            textField: ['get', 'point_count_abbreviated'],
+                            textColor: '#000',
+                            textSize: 12,
+                        }} />
+                    <CircleLayer id="clusterMusics"
+                        filter={['has', 'point_count']}
+                        style={{
+                            circlePitchAlignment: 'map',
+                            circleColor: '#8D939C',
+                            circleRadius: ['interpolate', ['linear'], ['get', 'point_count'], 2, 15, 5, 17, 10, 20, 15, 25, 20, 30],
+                            circleOpacity: 0.4,
+                            circleStrokeColor: '#8D939C',
+                            circleStrokeWidth: 1,
+                        }} />
+                    <SymbolLayer id="pointMusic"
+                        filter={['!', ['has', 'point_count']]}
+                        style={{
+                            iconImage: 'iconMusic',
+                            iconSize: 0.04,
+                            iconAnchor: 'bottom-left',
+                            iconOpacity: 0.8,
+                        }} />
+                    <Images images={{ iconMusic }} />
+                </ShapeSource>
+
+                <ShapeSource id="pointsLives" cluster shape={locationLives}>
+                    <SymbolLayer id="clusterLives-count"
+                        filter={['has', 'point_count']}
+                        style={{
+                            textField: ['get', 'point_count_abbreviated'],
+                            textColor: '#000',
+                            textSize: 12,
+                        }} />
+                    <CircleLayer id="clusterLives"
+                        filter={['has', 'point_count']}
+                        style={{
+                            circlePitchAlignment: 'map',
+                            circleColor: '#662d91',
+                            circleRadius: ['interpolate', ['linear'], ['get', 'point_count'], 2, 15, 5, 17, 10, 20, 15, 25, 20, 30],
+                            circleOpacity: 0.4,
+                            circleStrokeColor: '#662d91',
+                            circleStrokeWidth: 1,
+                        }} />
+                    <Images images={{ iconLive }} />
                 </ShapeSource>
             </MapView>
         </View>
