0b4c3796ccab294694b532a5510c9222a9127924
UnknownMaillet <62292183+arnaudmaillet@users.noreply.github.com>
2024-10-06 18:07:09 +0200

    Feat(video): add video information (account name, likes, comments ...)

--- screens/Video.tsx
+++ screens/Video.tsx
@@ -1,9 +1,10 @@
-import { ActivityIndicator, StyleSheet, TouchableWithoutFeedback, View, Image } from 'react-native';
+import { ActivityIndicator, StyleSheet, TouchableWithoutFeedback, View, Image, Text } from 'react-native';
 import React, { useState, useEffect, useRef } from 'react';
 import { Video, ResizeMode, AVPlaybackStatus } from 'expo-av';
-import Animated, { FadeIn, FadeOut, useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import Animated, { Easing, FadeIn, FadeOut, useSharedValue, useAnimatedStyle, withTiming, withRepeat } from 'react-native-reanimated';
 
 import { getVideoSource } from '~/assets/assets';
+import { FontAwesome6, MaterialIcons } from '@expo/vector-icons';
 
 interface VideoProps {
     id: number,
@@ -13,7 +14,7 @@ interface VideoProps {
     comments: number,
     shares: number,
     views: number,
-    saved: number,
+    bookmark: number,
     description: string
 }
 
@@ -28,7 +29,8 @@ const VideoScreen: React.FC<VideoScreenProps> = ({ video }) => {
     const progress = useSharedValue(0); // Progression de la vidéo partagée
     const videoRef = useRef<Video | null>(null); // Référence à la vidéo
 
-    // Gestion du chargement de la vidéo pour des transitions plus fluides
+    const bounceValue = useSharedValue(0);
+
     useEffect(() => {
         const loadVideo = async () => {
             if (videoRef.current) {
@@ -55,10 +57,30 @@ const VideoScreen: React.FC<VideoScreenProps> = ({ video }) => {
         }
     }, [isLoaded]);
 
+    useEffect(() => {
+        bounceValue.value = withRepeat(
+            withTiming(-5, {
+                duration: 1500,
+                easing: Easing.inOut(Easing.ease),
+            }),
+            -1, // Répéter indéfiniment
+            true // Inverser à chaque itération pour l'effet de retour
+        );
+    }, []);
+
     const animatedProgressBarStyle = useAnimatedStyle(() => ({
         width: `${progress.value * 100}%`,
     }));
 
+    const animatedArrowStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateY: bounceValue.value, // Appliquer la translation à l'icône
+                },
+            ],
+        };
+    })
 
     const handleVideoPress = async () => {
         console.log('Video pressed');
@@ -77,8 +99,7 @@ const VideoScreen: React.FC<VideoScreenProps> = ({ video }) => {
             const progressValue = status.positionMillis / status.durationMillis;
             progress.value = withTiming(progressValue, { duration: 500 }); // Animation fluide avec timing
         }
-    }
-
+    };
 
     return (
         <Animated.View
@@ -105,8 +126,46 @@ const VideoScreen: React.FC<VideoScreenProps> = ({ video }) => {
                     />
                 </TouchableWithoutFeedback>
 
+                {/* ProgressBar */}
                 <View style={styles.progressBarContainer}>
                     <Animated.View style={[styles.progressBar, animatedProgressBarStyle]} />
+                </View>
+
+                {/* Informations du compte en haut de l'écran */}
+                <View style={styles.accountInfoContainer}>
+                    <Text style={styles.accountText}>@{video.account}</Text>
+                    <Text style={styles.descriptionText}>{video.description}</Text>
+                </View>
+
+
+                {/* Icône avec l'animation de rebond */}
+                <Animated.View style={[styles.arrowContainer, animatedArrowStyle]}>
+                    <MaterialIcons name="keyboard-arrow-up" size={30} color="white" />
+                </Animated.View>
+
+
+                {/* Informations sociales : likes, commentaires, partages */}
+                <View style={styles.socialInfoContainer}>
+                    <View style={styles.socialItem}>
+                        <FontAwesome6 name="eye" size={20} color="white" />
+                        <Text style={styles.socialText}>{video.views}</Text>
+                    </View>
+                    <View style={styles.socialItem}>
+                        <FontAwesome6 name="heart" size={20} color="white" />
+                        <Text style={styles.socialText}>{video.likes}</Text>
+                    </View>
+                    <View style={styles.socialItem}>
+                        <FontAwesome6 name="bookmark" size={19} color="white" />
+                        <Text style={styles.socialText}>{video.bookmark}</Text>
+                    </View>
+                    <View style={styles.socialItem}>
+                        <FontAwesome6 name="share" size={20} color="white" />
+                        <Text style={styles.socialText}>{video.shares}</Text>
+                    </View>
+                    <View style={styles.socialItem}>
+                        <FontAwesome6 name="comment" size={20} color="white" />
+                        <Text style={styles.socialText}>{video.comments}</Text>
+                    </View>
                 </View>
             </View>
         </Animated.View>
@@ -143,7 +202,7 @@ const styles = StyleSheet.create({
         height: 5,
         backgroundColor: '#ccc',
         position: 'absolute',
-        bottom: 0, // Positionne la barre en bas de l'écran
+        bottom: 0,
         left: 0,
         right: 0,
     },
@@ -151,6 +210,58 @@ const styles = StyleSheet.create({
         height: '100%',
         backgroundColor: 'grey',
         borderRadius: 5,
+    },
+    accountInfoContainer: {
+        position: 'absolute',
+        top: 30,
+        left: 20,
+        right: 20,
+        alignItems: 'flex-start',
+    },
+    accountText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    descriptionText: {
+        color: 'white',
+        fontSize: 14,
+    },
+    arrowContainer: {
+        position: 'absolute',
+        bottom: 40,
+        left: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    socialInfoContainer: {
+        position: 'absolute',
+        bottom: 5,
+        left: 0,
+        right: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+        padding: 10,
+        borderTopLeftRadius: 2,
+        borderTopRightRadius: 2,
+    },
+    socialItem: {
+        width: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    socialText: {
+        width: 30,
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 12,
+        marginLeft: 2,
     },
 });
 
